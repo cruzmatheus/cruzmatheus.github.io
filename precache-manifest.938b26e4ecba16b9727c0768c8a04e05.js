cdd7397e5500b92b3855b862b7da107ae6219694
Matheus Cruz <contato.mscruz@gmail.com>
2019-03-05 22:50:32 +0000

    Publishing first version

---
+++ precache-manifest.938b26e4ecba16b9727c0768c8a04e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "564bada4b8f670bb14ff",
+    "url": "/static/css/main.23879290.chunk.css"
+  },
+  {
+    "revision": "564bada4b8f670bb14ff",
+    "url": "/static/js/main.564bada4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed3389a9c1bdabde08d61d06103f3eb3",
+    "url": "/index.html"
+  }
+];
